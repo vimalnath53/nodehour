9ac0c89ea41726b79730f6aa120fe6746911cee9
vimalnath53 <vimalnath53@gmail.com>
2017-06-06 17:42:22 +0800

    Update all.js

--- public/js/all.js
+++ public/js/all.js
@@ -6,3 +6,12 @@ function cancelAdd(){
     
     window.location.href = '/customers';
 }
+function deletecust(id) {
+	var res = confirm("Are you sure you want to delete this user");
+	if(res == true) {
+		window.location.href = '/customers/delete?id='+id;
+	} else {
+		return false;
+	}
+ 	
+}
